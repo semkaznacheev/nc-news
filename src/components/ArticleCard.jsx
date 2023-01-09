65f86e4f9707054093979daa93b3a0239d50c453
Semen Kaznacheev <semkaznacheev@gmail.com>
2023-01-09 15:36:28 +0000

    Made changes regarding comments

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -6,18 +6,13 @@ return ( <div className="Article_Card">
     <p>Title: {title}</p>
     <p>Topic: {topic}</p>
     <p>Author: {author}</p>
-     <div>
         <button>
             Like
         </button>
-     </div>
      <p>Current likes: {votes}</p>
-     <div>
         <button>
             Dislike
         </button>
-     </div>
-
 </div>
 )
 }
