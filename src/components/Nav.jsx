2e2bff26e6b5df702d1d59e8558278fa415e1941
Semen Kaznacheev <semkaznacheev@gmail.com>
2023-01-10 16:55:39 +0000

    Added some CSS

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 const Nav = () => {
     return (
     <nav className="Nav">
-        <h2>Navigation</h2>
         <Link to="/articles"> All Articles</Link>
        <div className="dropdown">
        <button className="dropbtn">Dropdown</button>
