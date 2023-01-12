63cc60614f1c340d9331ac199419a4e6aaed951e
Semen Kaznacheev <semkaznacheev@gmail.com>
2023-01-12 15:51:41 +0000

    TASK 10 is ready for review

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -10,23 +10,43 @@ const ArticlesList = () => {
     
 const [isLoading, setIsLoading] = useState(true);
 const [articles, setArticles] = useState([]);
+const [sortBy, setSortBy] = useState('created_at');
+const [order, setOrder] = useState('asc')
 
 const { topic } = useParams();
 
 useEffect(() => {
     setIsLoading(true);
-    getArticles(topic).then(({articles}) => {
+    getArticles(topic, sortBy, order).then(({articles}) => {
         setArticles(articles)
         setIsLoading(false)
     })
-}, [topic]);
-
+}, [topic, sortBy, order]);
 
 if (isLoading) {
     return <p className="Loading">Loading...</p>
 } 
 
 return (
+    <main>
+        <section>
+           <select value={sortBy} onChange={(e) => {
+                setSortBy(e.target.value) 
+                }}>
+                <option value="created_at"> Date </option>
+                <option value="votes"> Likes Quantity</option>
+                <option value="comment_count" > Comments Quantity</option> 
+            </select>
+        </section>
+        <section>
+           <select value={order} onChange={(e) => {
+                setOrder(e.target.value) 
+                }}>
+                <option value="asc"> Ascending </option>
+                <option value="desc"> Descending </option>
+            </select>
+        </section>
+        
     
  
     <ul className="Article_List"  style={{ backgroundImage:`url(${image})`}}>
@@ -40,7 +60,7 @@ return (
             )
         })}
     </ul>
-   
+    </main>
   
 )
 
